e599c9e639c8a0eb091984e247c3bb89fdd6653f
PickHD <taufikjanuar35@gmail.com>
2020-12-09 11:32:23 +0700

    Add Requester & Short Controllers

---
+++ api/controllers/short.controller.js
@@ -0,0 +1,25 @@
+const { Short } = require("../models");
+
+
+exports.getAllUrl = async (req, res, next) => {
+    try {
+    
+        //TODO : Create Dynamic Query String For Filter & Searching Rows
+        //! const {} = req.query 
+
+        const { count, rows } = await Short.findAndCountAll({ raw: true });
+
+        return res.json({
+            success: true,
+            data: {
+                count: count,
+                rows: rows
+            }
+        });
+
+    } catch (e) {
+        res.statusCode = 500;
+        return next(e);
+    }
+
+};

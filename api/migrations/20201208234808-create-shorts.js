dacf660a2cb1d3367ba5297bfb48c3f8790f82f8
PickHD <taufikjanuar35@gmail.com>
2020-12-09 11:28:25 +0700

    Fixed single quote to double & Add default value

--- api/migrations/20201208234808-create-shorts.js
+++ api/migrations/20201208234808-create-shorts.js
@@ -1,37 +1,39 @@
-'use strict';
+"use strict";
+const {nanoid} = require("nanoid");
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Shorts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      full_url: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      short_url: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      clicked: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: 0
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Shorts');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable("Shorts", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            full_url: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            short_url: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue:nanoid(7)
+            },
+            clicked: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable("Shorts");
+    }
 };

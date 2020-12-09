800de8f76a3ee7527c2b91be866767bd3678cc1e
PickHD <taufikjanuar35@gmail.com>
2020-12-09 10:54:55 +0700

    Add Model And Migration For Requesters Table

---
+++ api/models/requesters.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Requesters extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Requesters.init({
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Requesters',
+  });
+  return Requesters;
+};

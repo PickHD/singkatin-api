6503c60135e77c57b308d035a823a242d1ba59ae
PickHD <taufikjanuar35@gmail.com>
2020-12-09 14:52:14 +0700

    Fix indent and qoutes

--- api/migrations/20201209001418-create-tokens.js
+++ api/migrations/20201209001418-create-tokens.js
@@ -1,42 +1,42 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Tokens', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      token: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      requester_id: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-    await queryInterface.addConstraint('Tokens', {
-      fields: ['requester_id'],
-      type: 'foreign key',
-      name: 'requester_fkey_constraint',
-      references: { 
-        table: 'Requesters',
-        field: 'id'
-      },
-      onDelete: 'cascade',
-      onUpdate: 'cascade'
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Tokens');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable("Tokens", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            token: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            requester_id: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+        await queryInterface.addConstraint("Tokens", {
+            fields: ["requester_id"],
+            type: "foreign key",
+            name: "requester_fkey_constraint",
+            references: { 
+                table: "Requesters",
+                field: "id"
+            },
+            onDelete: "cascade",
+            onUpdate: "cascade"
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable("Tokens");
+    }
 };
--- api/models/tokens.js
+++ api/models/tokens.js
@@ -1,24 +1,24 @@
-'use strict';
+"use strict";
 const {
-  Model
-} = require('sequelize');
+    Model
+} = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Tokens extends Model {
+    class Tokens extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+        static associate(models) {
+            // define association here
+        }
     }
-  };
-  Tokens.init({
-    token: DataTypes.STRING,
-    requester_id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Tokens',
-  });
-  return Tokens;
+    Tokens.init({
+        token: DataTypes.STRING,
+        requester_id: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: "Tokens",
+    });
+    return Tokens;
 };

3c71c58f19919877e111218c9389e9d70f0bd863
PickHD <taufikjanuar35@gmail.com>
2020-12-09 14:39:01 +0700

    Commented temporary

--- api/routes/short.route.js
+++ api/routes/short.route.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 
 //!IMPORT CONTROLLER & MIDDLEWARE
 const { getAllUrl, createShortUrlHandler, getRedirectUrl, delOneShortUrlHandler } = require("../controllers/short.controller");
-const { verifyApiKey } = require("../middlewares/verifyApiKey.middleware");
+const verifyApiKey = require("../middlewares/verifyApiKey.middleware");
+const { fetchLimiter } = require("../middlewares/rateLimitHandler.middleware");
 
-router.get("/", [verifyApiKey], getAllUrl);
-router.post("/", [verifyApiKey], createShortUrlHandler);
-router.get("/:shortUrl", [verifyApiKey], getRedirectUrl);
-router.delete("/:shortUrl", [verifyApiKey], delOneShortUrlHandler);
+router.get("/", [verifyApiKey, fetchLimiter], getAllUrl);
+// router.post("/", [verifyApiKey], createShortUrlHandler);
+// router.get("/:shortUrl", [verifyApiKey,createLimiter], getRedirectUrl);
+// router.delete("/:shortUrl", [verifyApiKey], delOneShortUrlHandler);
 
 module.exports = router;

4f9e0cbccd44e535a7bd37a33be22cfe47a14617
PickHD <taufikjanuar35@gmail.com>
2020-12-10 13:39:01 +0700

    Uncomm. route post & add more validations

--- api/routes/short.route.js
+++ api/routes/short.route.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
+const { body } = require("express-validator");
 
 //!IMPORT CONTROLLER & MIDDLEWARE
 const { getAllUrl, createShortUrlHandler, getRedirectUrl, delOneShortUrlHandler } = require("../controllers/short.controller");
 const verifyApiKey = require("../middlewares/verifyApiKey.middleware");
-const { fetchLimiter } = require("../middlewares/rateLimitHandler.middleware");
+const { fetchLimiter, createLimiter } = require("../middlewares/rateLimitHandler.middleware");
+
+
 
 router.get("/", [verifyApiKey, fetchLimiter], getAllUrl);
-// router.post("/", [verifyApiKey], createShortUrlHandler);
+
+router.post("/", [verifyApiKey, createLimiter,body("full_url").not().isEmpty().isLength({max:255}).isURL()], createShortUrlHandler);
+
 // router.get("/:shortUrl", [verifyApiKey,createLimiter], getRedirectUrl);
 // router.delete("/:shortUrl", [verifyApiKey], delOneShortUrlHandler);
 

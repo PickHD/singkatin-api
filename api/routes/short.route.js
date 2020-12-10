ffc97f81270fe2f5dc48c1128a7e9a928319c528
PickHD <taufikjanuar35@gmail.com>
2020-12-10 18:43:05 +0700

    Uncommenting & add more validations

--- api/routes/short.route.js
+++ api/routes/short.route.js
@@ -6,11 +6,10 @@ const { getAllUrl, createShortUrlHandler, getRedirectUrl, delOneShortUrlHandler 
 const verifyApiKey = require("../middlewares/verifyApiKey.middleware");
 const { fetchLimiter, createLimiter } = require("../middlewares/rateLimitHandler.middleware");
 
-
 router.get("/", [verifyApiKey, fetchLimiter], getAllUrl);
 router.post("/", [verifyApiKey, createLimiter, body("full_url").not().isEmpty().isLength({ max: 255 }).isURL()], createShortUrlHandler);
 
 router.get("/:shortUrl", [verifyApiKey, fetchLimiter, param("shortUrl").not().isEmpty().isLength({ min: 7 }).isString()], getRedirectUrl);
-// router.delete("/:shortUrl", [verifyApiKey, param("shortUrl").not().isEmpty().isLength({ min: 7 }).isString()], delOneShortUrlHandler);
+router.delete("/:shortUrl", [verifyApiKey, createLimiter, param("shortUrl").not().isEmpty().isLength({ min: 7 }).isString()], delOneShortUrlHandler);
 
 module.exports = router;

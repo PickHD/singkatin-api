ac6cd9d5ecc66a33a8999321bcd7fddd3e581038
PickHD <taufikjanuar35@gmail.com>
2020-12-10 10:27:24 +0700

    Uncommenting & add more middleware validation

--- api/routes/requester.route.js
+++ api/routes/requester.route.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+const { body } = require("express-validator");
 
 //!IMPORT CONTROLLER & MIDDLEWARE
-const { createRequestApiKeyHandler } = require("../controllers/short.controller");
+const { createRequestApiKeyHandler } = require("../controllers/requester.controller");
 const { createLimiter } = require("../middlewares/rateLimitHandler.middleware");
 
-// router.post("/", [createLimiter], createRequestApiKeyHandler);
+router.post("/", [createLimiter, body("email").not().isEmpty(), body("email").isEmail().trim(), body("email").isLength({ min: 6 })], createRequestApiKeyHandler);
 
 module.exports = router;

6f9b640644c3ba759e1198db614185c6cde7ec6d
PickHD <taufikjanuar35@gmail.com>
2020-12-10 19:47:12 +0700

    Fix typo from "an hour" to "an 15 minutes"

--- api/middlewares/rateLimitHandler.middleware.js
+++ api/middlewares/rateLimitHandler.middleware.js
@@ -3,12 +3,12 @@ const rateLimit = require("express-rate-limit");
 const fetchLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
-    message: { success: false, err_code: 429, err_message: "Too many fetch requested, please try again after an hour", stack: null }
+    message: { success: false, err_code: 429, err_message: "Too many fetch requested, please try again after an 15 minutes", stack: null }
 });
 const createLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 5, // limit each IP to 5 requests per windowMs
-    message: { success: false, err_code: 429, err_message: "Too many tokens/request created, please try again after an hour", stack: null }
+    message: { success: false, err_code: 429, err_message: "Too many tokens/request created, please try again after an 15 minutes", stack: null }
 });
 
 module.exports = {

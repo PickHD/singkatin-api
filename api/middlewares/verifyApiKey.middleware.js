9dd6aac03eb93d908302bf4edcbd8c23be1b5ee5
PickHD <taufikjanuar35@gmail.com>
2020-12-14 20:13:36 +0700

    Improve code

--- api/middlewares/verifyApiKey.middleware.js
+++ api/middlewares/verifyApiKey.middleware.js
@@ -3,15 +3,17 @@ const { Token } = require("../models");
 const verifyApiKey = async (req, res, next) => {
     try {
         const apiKey = req.header("X-API-KEY");
+
+        if (apiKey === undefined) {
+            res.statusCode = 400;
+            return next(new Error("No Token Provided."));
+        }
+        
         const getToken = await Token.findOne({
             where: {
                 token: apiKey
             }
         });
-        if (!apiKey) {
-            res.statusCode = 400;
-            return next(new Error("No Token Provided."));
-        }
         if (!getToken) {
             res.statusCode = 401;
             return next(new Error("Key Invalid. Maybe Expired"));
